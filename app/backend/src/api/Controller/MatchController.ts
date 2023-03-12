ed96e863fc8aa62d92b543dcc1d91e91f31eb01e
Gustavo Tardin <gustavofilipetardin@outlook.com>
2023-03-12 12:16:24 -0300

    refactor: tira função comentada e melhora redundância na função getAll dao arquivo MatchController.ts

--- app/backend/src/api/Controller/MatchController.ts
+++ app/backend/src/api/Controller/MatchController.ts
@@ -12,22 +12,10 @@ class MatchController {
     return this._service;
   }
 
-  /* findByProgress = async (req: Request, res: Response) => {
-    const { q } = req.query;
-    const { type, message } = await this.service.filteredByProgress(q === 'true');
-    if (type) return res.status(type).json({ message });
-    return res.status(200).json(message);
-  };
-  */
-
   getAll = async (req: Request, res: Response) => {
     const { inProgress } = req.query;
 
-    // if (q) return this.findByProgress(req, res);
-
-    // console.log(q);
-
-    const { type, message } = inProgress !== undefined
+    const { type, message } = inProgress
       ? await this.service.filteredByProgress(inProgress === 'true') : await this.service.getAll();
     if (type) return res.status(type).json({ message });
     return res.status(200).json(message);
